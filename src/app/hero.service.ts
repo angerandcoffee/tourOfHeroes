b9a2ab91411fdd08ca5c7b5b54fca86ad4d0d26c
Lukasz Siwek <xofczarx@gmail.com>
2017-05-12 10:43:06 +0200

    HeroDetailComponent was revised.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,6 +4,11 @@ import { Hero } from "./hero";
 
 @Injectable()
 export class HeroService {
+    
+    getHero(id: number): Promise<Hero> {
+        return this.getHeroes()
+                .then(heroes => heroes.find(hero => hero.id === id));
+    }  
     getHeroes(): Promise<Hero[]> {
         return Promise.resolve(HEROES);
     }
@@ -13,4 +18,5 @@ export class HeroService {
         setTimeout(() => resolve(this.getHeroes()), 2000);
         });
     }
+
 }
